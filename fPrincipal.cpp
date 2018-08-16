2b0a08b3f1f02f6093b1791ebe45363f8ba264d0
Willian <willian.agostini94@gmail.com>
2018-08-16 20:35:12 -0300

    initial commit

---
+++ fPrincipal.cpp
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "fPrincipal.h"
+// ---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TfrmPrincipal *frmPrincipal;
+
+// ---------------------------------------------------------------------------
+__fastcall TfrmPrincipal::TfrmPrincipal(TComponent* Owner) : TForm(Owner) {
+}
+// ---------------------------------------------------------------------------
+
+void __fastcall TfrmPrincipal::btnListarPortasClick(TObject *Sender) {
+	ListarPortas();
+}
+// ---------------------------------------------------------------------------
+
+void TfrmPrincipal::ListarPortas() {
+	TRegistry *registro = new TRegistry;
+	TStringList *lista = new TStringList;
+
+	registro->RootKey = HKEY_LOCAL_MACHINE;
+	registro->OpenKey("HARDWARE\\DEVICEMAP\\SERIALCOMM", false);
+
+};
+// ---------------------------------------------------------------------------
